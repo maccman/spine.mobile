e16df205c827fefc993d570a9217c802448d7511
Alex MacCaw <maccman@gmail.com>
2011-09-15 12:54:29 +0100

    Hem does not have support for package.json/main attribute

---
+++ index.js
@@ -0,0 +1,7 @@
+(function() {
+  require('spine');
+  require('spine/lib/manager');
+  require('./lib/touch');
+  exports.Stage = require('./lib/stage');
+  exports.Panel = require('./lib/panel');
+}).call(this);
--- src/index.coffee
+++
@@ -1,6 +0,0 @@
-require('spine')
-require('spine/lib/manager')
-
-require('./touch')
-exports.Stage = require('./stage')
-exports.Panel = require('./panel')
